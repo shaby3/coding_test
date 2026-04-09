b7a504323ab85d3a5e2e5268876ada994d4cc056
shaby3 <119297658+shaby3@users.noreply.github.com>
2026-04-09 14:43:19 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

--- 260409/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.py
+++ 260409/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.py
@@ -4,6 +4,12 @@ N = int(input())
 
 
 def func(pre2, pre1, n):
+    if n == 1:
+        return pre2
+
+    if n == 2:
+        return pre1
+    
     pre2, pre1 = pre1, (pre2 * pre1) % 100
     if n == N:
         return pre1
