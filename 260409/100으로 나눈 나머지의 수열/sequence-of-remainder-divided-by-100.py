bf5774343aa6cd5eb828549aea600c86ddba70c6
shaby3 <119297658+shaby3@users.noreply.github.com>
2026-04-09 14:44:47 +0900

    Title: 100으로 나눈 나머지의 수열, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260409/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.py
+++ 260409/100으로 나눈 나머지의 수열/sequence-of-remainder-divided-by-100.py
@@ -3,16 +3,15 @@ N = int(input())
 # Please write your code here.
 
 
-def func(pre2, pre1, n):
-    if n == 1:
+def func(pre2, pre1, n, N):
+    if N == 1:
         return pre2
-
-    if n == 2:
+    if N == 2:
         return pre1
     
     pre2, pre1 = pre1, (pre2 * pre1) % 100
     if n == N:
         return pre1
-    return func(pre2, pre1, n + 1)
+    return func(pre2, pre1, n + 1, N)
 
-print(func(2, 4, 3))
+print(func(2, 4, 3, N))
