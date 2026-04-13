572fa840f27d3145d3c108653a8600d730b8a070
shaby3 <119297658+shaby3@users.noreply.github.com>
2026-04-13 16:53:18 +0900

    Title: 007, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260413/007/007.py
@@ -0,0 +1,16 @@
+secret_code, meeting_point, time = input().split()
+time = int(time)
+
+# Please write your code here.
+class Infrom:
+    def __init__(self, secret_code, meeting_point, time):
+        self.secret_code = secret_code
+        self.meeting_point = meeting_point
+        self.time = time
+
+
+inf = Inform(secret_code, meeting_point, time)
+
+print(f"secret code : {inf.secret_code}")
+print(f"meeting point : {inf.meeting_point}")
+print(f"secret code : {inf.time}")
