1867d7597b92cdb07dd514ecdb161ec5ed17de0b
shaby3 <119297658+shaby3@users.noreply.github.com>
2026-04-13 16:53:45 +0900

    Title: 007, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260413/007/007.py
+++ 260413/007/007.py
@@ -2,7 +2,7 @@ secret_code, meeting_point, time = input().split()
 time = int(time)
 
 # Please write your code here.
-class Infrom:
+class Inform:
     def __init__(self, secret_code, meeting_point, time):
         self.secret_code = secret_code
         self.meeting_point = meeting_point
@@ -13,4 +13,4 @@ inf = Inform(secret_code, meeting_point, time)
 
 print(f"secret code : {inf.secret_code}")
 print(f"meeting point : {inf.meeting_point}")
-print(f"secret code : {inf.time}")
+print(f"time : {inf.time}")
