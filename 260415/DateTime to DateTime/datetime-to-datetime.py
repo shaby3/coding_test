62f19af569e0987d3546892495584d88730b90d7
shaby3 <119297658+shaby3@users.noreply.github.com>
2026-04-15 10:26:20 +0900

    Title: DateTime to DateTime, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260415/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,13 @@
+a, b, c = map(int, input().split())
+
+# Please write your code here.
+# 하루는 총 몇분이지? 24시간 * 60분
+min_per_day = 24 * 60
+
+# 일 수 기준으로 소요 분 계산
+day_time = min_per_day * (a - 11)
+
+# 시, 분을 활용한 소요 분 계산
+min_time = (b * 60 + c) - (11 * 60 + 11)
+
+print(day_time + min_time)
