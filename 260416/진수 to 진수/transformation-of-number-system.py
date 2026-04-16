edefa11eda207de30963754249b5721b7196a76c
shaby3 <119297658+shaby3@users.noreply.github.com>
2026-04-16 13:46:29 +0900

    Title: 진수 to 진수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260416/진수 to 진수/transformation-of-number-system.py
+++ 260416/진수 to 진수/transformation-of-number-system.py
@@ -4,7 +4,14 @@ n = input()
 # Please write your code here.
 # a진수 -> 10진수 -> b진수
 
-num_10 = int(n, a)
+# 방법 1
+# num_10 = int(n, a)
+# 방법 2
+num_list = list(map(int, n))
+
+num_10 = 0
+for x in num_list:
+    num_10 = num_10 * a + x
 
 ans = []
 while num_10:
