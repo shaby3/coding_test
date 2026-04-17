edc635aab0770b9ecc9a89c62e39f6bb12423cd7
shaby3 <119297658+shaby3@users.noreply.github.com>
2026-04-17 23:35:32 +0900

    Title: 연속되는 수 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260417/연속되는 수 2/continuous-number2.py
+++ 260417/연속되는 수 2/continuous-number2.py
@@ -2,9 +2,13 @@ n = int(input())
 arr = [int(input()) for _ in range(n)]
 
 # Please write your code here.
-# ans = 1
+ans = 1
 cnt = 1
 for i in range(n - 1):
-    if arr[i] != arr[i + 1]:
+    if arr[i] == arr[i + 1]:
         cnt += 1
-print(cnt)
+        ans = max(ans, cnt)
+    else:
+        cnt = 1
+
+print(ans)
