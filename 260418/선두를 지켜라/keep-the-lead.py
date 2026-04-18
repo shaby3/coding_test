274fad5dae633580f0af426d5f45e8b5ec372558
shaby3 <119297658+shaby3@users.noreply.github.com>
2026-04-18 22:20:41 +0900

    Title: 선두를 지켜라, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260418/선두를 지켜라/keep-the-lead.py
@@ -0,0 +1,47 @@
+n, m = map(int, input().split())
+
+# Process A's movements
+v = []
+t = []
+for _ in range(n):
+    vi, ti = map(int, input().split())
+    v.append(vi)
+    t.append(ti)
+
+# Process B's movements
+v2 = []
+t2 = []
+for _ in range(m):
+    vi, ti = map(int, input().split())
+    v2.append(vi)
+    t2.append(ti)
+
+# Please write your code here.
+time1 = [0]
+time2 = [0]
+
+for velocity, time in zip(v, t):
+    for _ in range(time):
+        time1.append(velocity)
+
+for velocity, time in zip(v2, t2):
+    for _ in range(time):
+        time2.append(velocity)
+
+prev_state = 0
+ans = 0
+for idx in range(1, len(time1)):
+    if time1[idx] > time2[idx]:
+        state = 1
+        if prev_state == 2:
+            ans += 1
+        prev_state = state
+    elif time1[idx] < time2[idx]:
+        state = 2
+        if prev_state == 1:
+            ans += 1
+        prev_state = state
+    else:
+        prev_state = 0
+
+print(ans)
