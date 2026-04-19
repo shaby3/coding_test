ad77892b03b271d94e0bf1962b9ba4963a56a941
shaby3 <119297658+shaby3@users.noreply.github.com>
2026-04-19 17:18:46 +0900

    Title: 선두를 지켜라 3, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260419/선두를 지켜라 3/keep-the-lead-3.py
@@ -0,0 +1,22 @@
+N, M = map(int, input().split())
+
+# Process A's movements
+v = []
+t = []
+for _ in range(N):
+    vi, ti = map(int, input().split())
+    v.append(vi)
+    t.append(ti)
+
+# Process B's movements
+v2 = []
+t2 = []
+for _ in range(M):
+    vi, ti = map(int, input().split())
+    v2.append(vi)
+    t2.append(ti)
+
+# Please write your code here.
+time1 = [0]
+time2 = [0]
+for velocity, time in zip(v, t):
