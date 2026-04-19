ea5fc519f483cf6ef0015ecc90ea5232b9e6eade
shaby3 <119297658+shaby3@users.noreply.github.com>
2026-04-19 17:30:01 +0900

    Title: 선두를 지켜라 3, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260419/선두를 지켜라 3/keep-the-lead-3.py
+++ 260419/선두를 지켜라 3/keep-the-lead-3.py
@@ -19,4 +19,37 @@ for _ in range(M):
 # Please write your code here.
 time1 = [0]
 time2 = [0]
+
 for velocity, time in zip(v, t):
+    for _ in range(time):
+        time1.append(time1[-1] + velocity)
+
+for velocity, time in zip(v2, t2):
+    for _ in range(time):
+        time2.append(time2[-1] + velocity)
+
+if time1[1] > time2[1]:
+    prev = 1
+elif time1[1] < time2[1]:
+    prev = 2
+else:
+    prev = 3
+
+ans = 0
+for idx in range(2, len(time1)):
+    if time1[idx] > time2[idx]:
+        if prev != 1:
+            ans += 1
+        prev = 1
+    elif time1[idx] < time2[idx]:
+        if prev != 2:
+            ans += 1
+        prev = 2
+    else:
+        if prev != 3:
+            ans += 1
+        prev = 3
+
+print(ans + 1)
+# print(time1)
+# print(time2)
