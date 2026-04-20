b93bf66eaa1c0d1c56bc8ab7f3622ba27704e5af
shaby3 <119297658+shaby3@users.noreply.github.com>
2026-04-20 12:48:53 +0900

    Title: 문자에 따른 명령 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260420/문자에 따른 명령 2/text-based-commands2.py
@@ -0,0 +1,16 @@
+dirs = input()
+
+# Please write your code here.
+# 북쪽: 0, 1 동쪽: 1, 0, 남쪽: 0, -1, 서쪽: -1, 0
+
+dx = [0, 1, 0, -1]
+dy = [1, 0, -1, 0]
+
+d = 0
+for dir in dirs:
+    if dir == "L":
+        d = (d - 1) % 4
+    elif dir == "R":
+        d = (d + 1) % 4
+
+print(dx[d], dy[d])
