7c8a2389bdee37374525662b2478f013572435a4
shaby3 <119297658+shaby3@users.noreply.github.com>
2026-04-21 09:44:16 +0900

    Title: 모이자, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260421/모이자/gather.py
@@ -0,0 +1,13 @@
+n = int(input())
+A = list(map(int, input().split()))
+
+# Please write your code here.
+ans = 10000
+for dest in range(n):
+    total_dist = 0
+    for start in range(n):
+        dist = abs(dest - start)
+        total_dist += dist * A[start]
+    
+    ans = min(ans, total_dist)
+print(ans)
