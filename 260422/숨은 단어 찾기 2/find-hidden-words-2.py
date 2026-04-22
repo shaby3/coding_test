809b0b369ec86a160499aed11bdef6f59f0bda81
shaby3 <119297658+shaby3@users.noreply.github.com>
2026-04-22 15:13:57 +0900

    Title: 숨은 단어 찾기 2, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 260422/숨은 단어 찾기 2/find-hidden-words-2.py
+++ 260422/숨은 단어 찾기 2/find-hidden-words-2.py
@@ -1,140 +1,45 @@
-# N, M = map(int, input().split())
-# arr = [input() for _ in range(N)]
+N, M = map(int, input().split())
+arr = [input() for _ in range(N)]
 
-# # Please write your code here.
-
-# def in_range(x, y):
-#     return 0 <= x < N and 0 <= y < M
-
-# def func():
-#     ans = 0
-#     for i in range(N):
-#         for j in range(M):
-#             char = arr[i][j]
-#             if char == 'L': # char이 l일때만 검사하면 됨
-#                 # 가로 체크
-#                 for d in range(1, 3):
-#                     nj = j + d
-#                     if in_range(i, nj) and arr[i][nj] == 'E':
-#                         pass
-#                     else:
-#                         break
-#                 else:
-#                     ans += 1
-
-#                 for d in range(1, 3):
-#                     nj = j - d
-#                     if in_range(i, nj) and arr[i][nj] == 'E':
-#                         pass
-#                     else:
-#                         break
-#                 else:
-#                     ans += 1
-
-#                 # 세로 체크
-#                 for d in range(1, 3):
-#                     ni = i + d
-#                     if in_range(ni, j) and arr[ni][j] == 'E':
-#                         pass
-#                     else:
-#                         break
-#                 else:
-#                     ans += 1
-
-#                 for d in range(1, 3):
-#                     ni = i - d
-#                     if in_range(ni, j) and arr[ni][j] == 'E':
-#                         pass
-#                     else:
-#                         break
-#                 else:
-#                     ans += 1
-
-#                 # 우측 대각선 체크
-#                 for d in range(1, 3):
-#                     ni = i + d
-#                     nj = j + d
-#                     if in_range(ni, nj) and arr[ni][nj] == 'E':
-#                         pass
-#                     else:
-#                         break
-#                 else:
-#                     ans += 1
-
-#                 # 우측 대각선 체크
-#                 for d in range(1, 3):
-#                     ni = i - d
-#                     nj = j - d
-#                     if in_range(ni, nj) and arr[ni][nj] == 'E':
-#                         pass
-#                     else:
-#                         break
-#                 else:
-#                     ans += 1
-
-#                 # 좌측 대각선 체크
-#                 for d in range(1, 3):
-#                     ni = i + d
-#                     nj = j - d
-
-#                     if in_range(ni, nj) and arr[ni][nj] == 'E':
-#                         pass
-#                     else:
-#                         break
-#                 else:
-#                     ans += 1
-
-#                 for d in range(1, 3):
-#                     ni = i - d
-#                     nj = j + d
-
-#                     if in_range(ni, nj) and arr[ni][nj] == 'E':
-#                         pass
-#                     else:
-#                         break
-#                 else:
-#                     ans += 1
-#     return ans
-
-# print(func())
-                
-# 변수 선언 및 입력
-n, m = tuple(map(int, input().split()))
-arr = [
-    input()
-    for _ in range(n)
-]
+# Please write your code here.
 
 def in_range(x, y):
-    return 0 <= x and x < n and 0 <= y and y < m
+    return 0 <= x < N and 0 <= y < M
 
-dxs, dys = [1, 1, 1, -1, -1, -1, 0, 0], [-1, 0, 1, -1, 0, 1, -1, 1]
+def func():
+    dxs = [0, -1, -1, -1, 0, 1, 1, 1]
+    dys = [-1, -1, 0, 1, 1, 1, 0, -1]
+    ans = 0
+    for i in range(N):
+        for j in range(M):
+            num = arr[i][j]
+            
+            if num != 'L':
+                continue
+            
+            # 다른 문자가 나올 때 까지 개수를 세고 다른 문자
+            # 가 나왔다면 그 순간의 개수를 체크해서 3개면 ans +=1
+            for dx, dy in zip(dxs, dys):
+                curx = i
+                cury = j
+                cnt = 1
 
-# 모든 좌표에서 다 확인해봅니다.
-cnt = 0
-for i in range(n):
-	# 격자를 벗어나지 않을 범위로만 잡습니다.
-	for j in range(m):
-		
-		if arr[i][j] != 'L':
-			continue
-		
-		for dx, dy in zip(dxs, dys):
-			curt = 1
-			curx = i
-			cury = j
-			while True:
-				nx = curx + dx
-				ny = cury + dy
-				if in_range(nx, ny) == False:
-					break
-				if arr[nx][ny] != 'E':
-					break
-				curt += 1
-				curx = nx
-				cury = ny
-			
-			if curt >= 3:
-				cnt += 1
+                while True:
+                    nx = curx + dx
+                    ny = cury + dy
+                    if not in_range(nx, ny):
+                        break
+                    if arr[nx][ny] != 'E':
+                        break
+                    curx = nx
+                    cury = ny
+                    cnt += 1
+                
+                if cnt == 3:
+                    ans += 1
+    return ans
 
-print(cnt)
+print(func())
+
+
+            
