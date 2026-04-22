a9a5bbcce595f0106f6fa66192b7aee8478505c5
shaby3 <119297658+shaby3@users.noreply.github.com>
2026-04-22 14:18:15 +0900

    Title: 오목, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

--- 260422/오목/O-mok.py
+++ 260422/오목/O-mok.py
@@ -1,79 +1,65 @@
 board = [list(map(int, input().split())) for _ in range(19)]
 
 # Please write your code here.
-# 모든 좌표를 순회하며 우측, 우측대각선, 아래 방향을 탐색해서 
+# 모든 좌표를 순회하며 우측, 우측대각선, 아래, 좌측 대각선 아래 방향을 탐색해서 
 # 현재 좌표의 숫자와 다 같은 숫자인지 검증해서 맞으면 바둑돌과
 # 중간 바둑돌의 좌표를 return 해주는 함수 작성
 
-def check_row(si, sj):
-    num = board[si][sj]
-    if board[si][sj: sj + 5] == [num] * 5:
-        return num, (si, sj + 2)
-    return None
+def in_range(x, y):
+    return 0 <= x < 19 and 0 <= y < 19
 
-def check_col(si, sj):
-    num = board[si][sj]
-    if [board[si + d][sj] for d in range(5)] == [num] * 5:
-        return num, (si + 2, sj)
-    return None
+def func():
+    for i in range(len(board)):
+        for j in range(len(board[0])):
+            num = board[i][j]
+            if not num:
+                continue
+            
+            # 가로 체크
+            for d in range(1, 5):
+                nj = j + d
+                if in_range(i, nj) and board[i][nj] == num:
+                    pass
+                else:
+                    break
+            else:
+                return num, (i, j + 2)
 
-def check_diag(si, sj):
-    num = board[si][sj]
-    for d in range(1, 5):
-        ni, nj = si + d, sj + d
-        if board[ni][nj] != num:
-            return None
-    return num, (si + 2, sj + 2)
+            # 세로 체크
+            for d in range(1, 5):
+                ni = i + d
+                if in_range(ni, j) and board[ni][j] == num:
+                    pass
+                else:
+                    break
+            else:
+                return num, (i + 2, j)           
 
-def check_rev_diag(si, sj):
-    num = board[si][sj]
-    for d in range(1, 5):
-        ni, nj = si + d, sj - d
-        if board[ni][nj] != num:
-            return None
-    return num, (si + 2, sj - 2)
+            # 우측 대각선 체크
+            for d in range(1, 5):
+                ni = i + d
+                nj = j + d
+                if in_range(ni, nj) and board[ni][nj] == num:
+                    pass
+                else:
+                    break
+            else:
+                return num, (i + 2, j + 2)           
 
-flag = True
-# 가로부터
-for i in range(19):
-    for j in range(15):
-        if board[i][j] != 0:
-            val = check_row(i, j)
-            if val is not None:
-                num, (x, y) = val
-                print(num)
-                print(x + 1, y + 1)
-                flag = False
+            # 좌측 대각선 체크
+            for d in range(1, 5):
+                ni = i + d
+                nj = j - d
+                if in_range(ni, nj) and board[ni][nj] == num:
+                    pass
+                else:
+                    break
+            else:
+                return num, (i + 2, j - 2)
 
-for i in range(15):
-    for j in range(19):
-        if board[i][j] != 0:
-            val = check_col(i, j)
-            if val is not None:
-                num, (x, y) = val
-                print(num)
-                print(x + 1, y + 1)
-                flag = False
+num, coord = func()
 
-for i in range(15):
-    for j in range(15):
-        if board[i][j] != 0:
-            val = check_diag(i, j)
-            if val is not None:
-                num, (x, y) = val
-                print(num)
-                print(x + 1, y + 1)
-                flag = False
+print(num)
+print(coord[0] + 1, coord[1] + 1)
 
-for i in range(15):
-    for j in range(4, 19):
-        if board[i][j] != 0:
-            val = check_rev_diag(i, j)
-            if val is not None:
-                num, (x, y) = val
-                print(num)
-                print(x + 1, y + 1)
-                flag = False
 
-if flag:
-    print(0)
