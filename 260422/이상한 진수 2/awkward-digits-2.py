20c3863043f39e004cdffa29ca3b7a0c760dd0cd
shaby3 <119297658+shaby3@users.noreply.github.com>
2026-04-22 11:59:57 +0900

    Title: 이상한 진수 2, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260422/이상한 진수 2/awkward-digits-2.py
+++ 260422/이상한 진수 2/awkward-digits-2.py
@@ -1,9 +1,12 @@
-a = list(map(str, input()))
+a = list(map(int, input()))
 
 # Please write your code here.
+ans = 0
 for idx in range(len(a)):
-    if a[idx] != "1":
-        a[idx] = "1"
-        break
+    a[idx] = 1 - a[idx]
+    
+    ans = max(ans, int(''.join(map(str,a)), 2))
 
-print(int("".join(a), 2))
+    a[idx] = 1 - a[idx]
+
+print(ans)
