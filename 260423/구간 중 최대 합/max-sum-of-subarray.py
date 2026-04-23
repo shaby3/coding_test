35f6e92c1df5fed995a184a7de84732de2307566
shaby3 <119297658+shaby3@users.noreply.github.com>
2026-04-23 17:19:39 +0900

    Title: 구간 중 최대 합, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260423/구간 중 최대 합/max-sum-of-subarray.py
+++ 260423/구간 중 최대 합/max-sum-of-subarray.py
@@ -1,12 +1,29 @@
-n, k = map(int, input().split())
+# n, k = map(int, input().split())
+# arr = list(map(int, input().split()))
+
+# # Please write your code here.
+# summation = sum(arr[:k])
+# ans = summation
+# for j in range(k, n):
+#     summation += arr[j]
+#     summation -= arr[j - k]
+#     ans = max(ans, summation)
+
+# print(ans)
+
+# 변수 선언 및 입력
+n, k = tuple(map(int, input().split()))
 arr = list(map(int, input().split()))
 
-# Please write your code here.
-summation = sum(arr[:k])
-ans = summation
-for j in range(k, n):
-    summation += arr[j]
-    summation -= arr[j - k]
-    ans = max(ans, summation)
+# 모든 구간의 시작점을 잡아봅니다.
+max_sum = 0
+for i in range(0, n - k + 1):
+    # 해당 구간 내 원소의 합을 구합니다.
+    interval_sum = 0
+    for j in range(i, i + k):
+        interval_sum += arr[j]
 
-print(ans)
+    # 최댓값을 구합니다.
+    max_sum = max(max_sum, interval_sum)
+                        
+print(max_sum)
