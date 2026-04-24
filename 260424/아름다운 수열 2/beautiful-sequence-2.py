9fc58defbf993e6a12ebacc11cc729d96677a43c
shaby3 <119297658+shaby3@users.noreply.github.com>
2026-04-24 16:00:29 +0900

    Title: 아름다운 수열 2, Time: 0ms, Memory: 0MB, Status: Memory Limit Exceed - Codetree

--- 260424/아름다운 수열 2/beautiful-sequence-2.py
+++ 260424/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,10 +1,19 @@
+from itertools import permutations
+
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
 # Please write your code here.
+# perms = list(map(list, permutations(B)))
+perms = list(permutations(B))
+
+
 ans = 0
 for i in range(N - M + 1):
-    if set(A[i:i+M]) == set(B):
+    
+    if tuple(A[i:i+M]) in perms:
         ans += 1
+
+    
 print(ans)
