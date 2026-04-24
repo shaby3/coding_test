d4abdc74f64fd204b80a13336d37dee54862d79c
shaby3 <119297658+shaby3@users.noreply.github.com>
2026-04-24 16:05:38 +0900

    Title: 아름다운 수열 2, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

--- 260424/아름다운 수열 2/beautiful-sequence-2.py
+++ 260424/아름다운 수열 2/beautiful-sequence-2.py
@@ -1,19 +1,17 @@
-from itertools import permutations
+from 
 
 N, M = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
 # Please write your code here.
-# perms = list(map(list, permutations(B)))
+perms = list(map(list, permutations(B)))
 
 ans = 0
 for i in range(N - M + 1):
     cur_val = A[i: i + M]
-    for b in permutations(B):
-        if tuple(cur_val) == b:
-            ans += 1
-            break
-
-    
+    if cur_val in perms:
+        ans += 1
+        break
 print(ans)
+
