549e0190e06920a908057dac11828f9441e3c300
shaby3 <119297658+shaby3@users.noreply.github.com>
2026-04-28 13:17:57 +0900

    Title: 개발팀의 능력, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260428/개발팀의 능력/the-capabilities-of-the-development-team.py
@@ -0,0 +1,20 @@
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+total_abil = sum(arr)
+ans = float('inf')
+for i in range(5):
+    for j in range(i + 1, 5):
+        team1 = arr[i] + arr[j]
+        for k in range(5):
+            if k == i or k == j:
+                continue
+            for l in range(k + 1, 5):
+                if l == i or l == j:
+                    continue
+                team2 = arr[k] + arr[j]
+                team3 = total_abil - team2 - team1
+                ans = min(ans, max(team1, team2, team3) - min(team1, team2, team3))
+
+print(-1) if ans == 0 else print(ans)
+                
